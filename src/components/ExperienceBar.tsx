1d30be00168136e6a33edbad85fa5682ac8f3f44
Matheus Oliveira Mesquita <71468688+Harrys23@users.noreply.github.com>
2021-03-15 16:22:30 -0300

    Programações do dia 3 do MLW.

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,16 +1,28 @@
+import { useContext } from 'react'
+import { challengesContext } from '../contexts/ChallengesContexts'
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+	const { currentExperience, experienceToNextLevel } = useContext(challengesContext);
+
+	const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+
 	return (
 		<header className={styles.experienceBar}>
 			<span>0 xp</span>
 			<div>
-				<div style={{ width: '50%' }} />
+				<div style={{ width: `${percentToNextLevel}%` }} />
 
-				<span className={styles.currentExperience} style={{ left: '50%' }}>300xp</span>
+				<span
+					className={styles.currentExperience}
+					//desafio para mais tarde, colocar um balãozim envolta do xp atual.
+					style={{ left: `${percentToNextLevel}%` }}
+				>
+					{currentExperience} xp
+				</span>
 			</div>
 
-			<span>600 xp</span>
+			<span>{experienceToNextLevel} xp</span>
 		</header>
 	)
 }
